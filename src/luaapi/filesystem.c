da3bcc63f21745d85d3ab18a82607e1435fdca12
rnlf <rnlf@users.noreply.github.com>
2015-06-16 17:17:37 +0200

    Added love.filesystem.love

--- src/luaapi/filesystem.c
+++ src/luaapi/filesystem.c
@@ -21,7 +21,23 @@ static int l_filesystem_read(lua_State* state) {
   return 2;
 }
 
+
+static int l_filesystem_load(lua_State* state) {
+  char const* filename = l_tools_toStringOrError(state, 1);
+  char* data = NULL;
+  int len = filesystem_read(filename, &data);
+  if(len < 0) {
+    lua_pushstring(state, "could not read file");
+    return lua_error(state);
+  }
+
+  luaL_loadstring(state, data);
+  free(data);
+  return 1;
+}
+
 static luaL_Reg const regFuncs[] = {
+  {"load", l_filesystem_load},
   {"read", l_filesystem_read},
   {NULL, NULL}
 };
