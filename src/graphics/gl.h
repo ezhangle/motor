ef8a1af4f2002dabfb8a4c03954ea2ce70948736
rnlf <rnlf@users.noreply.github.com>
2015-08-27 11:25:55 +0200

    Fixed SDL include files

--- src/graphics/gl.h
+++ src/graphics/gl.h
@@ -3,10 +3,5 @@
 #define NO_SDL_GLEXT 
 #include <GL/glew.h> 
 #include <GL/gl.h>
-#ifdef SDL2_USED
-#include <SDL.h>
-#include <SDL_opengl.h>
-#else
-#include <SDL/SDL.h> 
-#include <SDL/SDL_opengl.h>
-#endif
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
--- src/keyboard.h
+++ src/keyboard.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <SDL_keyboard.h>
+#include <SDL2/SDL_keyboard.h>
 #include <stdbool.h>
 
 typedef struct {
